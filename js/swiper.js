355a32cec5e944e26591a634100eea8a0d3f0fda
i-webtest <stasbass80@gmail.com>
2023-03-01 21:49:38 +0300

    Created new project

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+
+  // slidesPerView: 4,
+  // spaceBetween: 20,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button_next',
+    prevEl: '.swiper-button_prev',
+  },
+});
